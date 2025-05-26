3c429234d31b4bb84e101631ce9fc5556dfb603d
JCGonzalez <johancamilogonzalez@ucundinamarca.edu.co>
2025-05-26 16:03:34 -0500

    +

--- netlify/functions/usuarios.js
+++ netlify/functions/usuarios.js
@@ -3,12 +3,12 @@ var cors = require("cors");
 var serverless = require ('serverless-http');
 var port = process.env.PORT || 5000;
 var app = express();
-var estudiantesroutes = require("../../backend/routes/estudiantesroutes.js");
+var usuroutes = require("../../backend/routes/usuariosrutas.js");
 app.use(express.json());
 app.use(cors());
 
 var router = express.Router();
-router.use ("/estudiantes",estudiantesroutes);
+router.use ("/usuarios",usuroutes);
 
 var handler = app.use ('/.netlify/functions',router);
 exports.handler = serverless (app);
